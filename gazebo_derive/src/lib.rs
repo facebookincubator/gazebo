d6e1b2de1752f1a43016ae507f92efae92f560ce
Neil Mitchell <ndmitchell@fb.com>
2020-10-14 00:09:43 -0700

    Improve the docs for derive
    Summary: `Dupe` in this crate links to the macro, which wasn't what we were going for. Add a preamble. Add notes on the ones that were undocumented
    
    Reviewed By: cjhopman
    
    Differential Revision: D24277361
    
    fbshipit-source-id: f7f804b03c43b4cf7390e0d47815aa9112c60a6b

--- gazebo_derive/src/lib.rs
+++ gazebo_derive/src/lib.rs
@@ -10,6 +10,9 @@
 #![allow(renamed_and_removed_lints)] // intra_doc_link_resolution_failure got renamed
 #![deny(intra_doc_link_resolution_failure)]
 
+//! Derivations for the [Gazebo library](https://docs.rs/gazebo/).
+//! Usually you would use these derivations via exports from that library.
+
 mod any_lifetime;
 mod clone;
 mod copy;
@@ -21,13 +24,13 @@ mod variant;
 
 extern crate proc_macro;
 
-/// Derive the [`Dupe` trait](Dupe).
+/// Derive the `Dupe` trait.
 #[proc_macro_derive(Dupe)]
 pub fn derive_dupe(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     dupe::derive_dupe(input)
 }
 
-/// Derive the [`Dupe` trait](Dupe), but without requiring all type arguments to implement [`Dupe`](Dupe).
+/// Derive the `Dupe` trait, but without requiring all type arguments to implement `Dupe`.
 #[proc_macro_derive(Dupe_)]
 pub fn derive_dupe_(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     dupe::derive_dupe_(input)
@@ -51,23 +54,26 @@ pub fn derive_default_(input: proc_macro::TokenStream) -> proc_macro::TokenStrea
     default::derive_default_(input)
 }
 
-/// Derive the [`AnyLifetime` trait](AnyLifetime). Requires the type has no type arguments, no constant arguments,
+/// Derive the `AnyLifetime` trait. Requires the type has no type arguments, no constant arguments,
 /// and at most one lifetime argument.
 #[proc_macro_derive(AnyLifetime)]
 pub fn derive_any_lifetime(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     any_lifetime::derive_any_lifetime(input)
 }
 
+/// Derive the `MaybeEq` trait.
 #[proc_macro_derive(MaybeEq)]
 pub fn derive_maybe_eq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     maybe_eq::derive_maybe_eq(input, true)
 }
 
+/// Derive the `MaybeEq` trait, but so that it is never comparable.
 #[proc_macro_derive(MaybeEq_Never)]
 pub fn derive_not_maybe_eq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     maybe_eq::derive_maybe_eq(input, false)
 }
 
+/// Derive the `VariantName` trait.
 #[proc_macro_derive(VariantName)]
 pub fn derive_variant_names(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     variant::derive_variant_names(input)

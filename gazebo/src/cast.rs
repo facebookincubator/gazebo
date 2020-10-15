236f1cb37597150c718d6ec85ff8ad11639e7f2f
Neil Mitchell <ndmitchell@fb.com>
2020-10-15 06:21:22 -0700

    Document the ways that cast causes undefined-behaviour
    Summary: These functions are inherently unsafe if you use the result, but some are undefined behaviour even if you don't, so document that.
    
    Reviewed By: pranjalssh
    
    Differential Revision: D24330677
    
    fbshipit-source-id: 893a6264f73edddb49eb58138116d6a075e793df

--- gazebo/src/cast.rs
+++ gazebo/src/cast.rs
@@ -10,20 +10,25 @@
 //! Cast between types with no conversion.
 //!
 //! Most of these operations are inherently unsafe, but provided as strongly-typed wrappers
-//! to reduce the chance of typos ending up with even more unsafe functions.
+//! to reduce the chance of typos ending up with even more unsafe functions. If you use the
+//! result in incorrect ways, it will cause undefined behaviour.
 
 pub fn ptr_to_usize<T: ?Sized>(x: &T) -> usize {
     x as *const T as *const () as usize
 }
 
+/// Undefined behaviour if the argument is zero, or does not satisfy the alignment
+/// of type `T`.
 pub unsafe fn usize_to_ptr<'a, T>(x: usize) -> &'a T {
     &*(x as *const T)
 }
 
+/// Undefined behaviour if the argument does not satisfy the alignment of type `To`.
 pub unsafe fn ptr<From, To>(x: &From) -> &To {
     &*(x as *const From as *const To)
 }
 
+/// Undefined behaviour if the argument does not satisfy the alignment of type `To`.
 pub unsafe fn ptr_mut<From, To>(x: &mut From) -> &mut To {
     &mut *(x as *mut From as *mut To)
 }

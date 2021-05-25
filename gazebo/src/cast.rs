00071414c6abf0088c1314304c97f4929de38590
Neil Mitchell <ndmitchell@fb.com>
2021-05-25 00:46:14 -0700

    Add transmute_unchecked
    Summary: I needed this function, and it's better to have it thoroughly documented and asserted than trying to hack it.
    
    Reviewed By: bobyangyf
    
    Differential Revision: D28625397
    
    fbshipit-source-id: f7be6c9012b4ba3451cad2d417920a21b7b7a716

--- gazebo/src/cast.rs
+++ gazebo/src/cast.rs
@@ -17,6 +17,8 @@
 // So doc's wouldn't really help.
 #![allow(clippy::missing_safety_doc)]
 
+use std::{mem, ptr};
+
 pub fn ptr_to_usize<T: ?Sized>(x: &T) -> usize {
     x as *const T as *const () as usize
 }
@@ -41,6 +43,22 @@ pub unsafe fn ptr_lifetime<'a, 'b, T: ?Sized>(x: &'a T) -> &'b T {
     &*(x as *const T)
 }
 
+/// Like normal [`transmute`](std::mem::transmute), but without the compile-time
+/// check that the sizes of the input and output are the same. Despite the removal
+/// of the compile-time check, this property _must still hold_, and there is
+/// a [`debug_assert_eq`] which checks that. All the rules and warnings from
+/// `transmute` still apply.
+///
+/// This function is mostly useful when you have types that are polymorphic,
+/// e.g. `Vec<T>`, that `transmute` cannot be applied to.
+pub unsafe fn transmute_unchecked<A, B>(x: A) -> B {
+    debug_assert_eq!(mem::size_of::<A>(), mem::size_of::<B>());
+    debug_assert_eq!(0, (&x as *const A).align_offset(mem::align_of::<B>()));
+    let b = ptr::read(&x as *const A as *const B);
+    mem::forget(x);
+    b
+}
+
 #[macro_export]
 /// `transmute!(from-type, to-type, value)` will do a [`transmute`](std::mem::transmute),
 /// but the original and result types must be specified.
@@ -52,6 +70,9 @@ macro_rules! transmute {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::cell::Cell;
+
     #[test]
     fn test_transmute() {
         #[allow(clippy::useless_transmute)]
@@ -59,5 +80,59 @@ mod tests {
             transmute!(&'a str, &'static str, x)
         }
         assert_eq!(unsafe { downcast_string("test") }, "test");
+    }
+
+    struct Foo<'a, T>(&'a Cell<isize>, &'a T);
+
+    impl<T> Drop for Foo<'_, T> {
+        fn drop(&mut self) {
+            self.0.set(self.0.get() - 1)
+        }
+    }
+
+    #[test]
+    fn test_transmute_unchecked() {
+        fn magic<'a, T>(x: T) -> Foo<'a, Vec<u8>> {
+            unsafe { transmute_unchecked(x) }
+        }
+
+        let s = "magic".to_owned();
+        let c = Cell::new(8);
+        let input = Foo(&c, &s);
+        let output = magic(input);
+        assert_eq!(c.get(), 8);
+        assert_eq!(output.1, "magic".as_bytes());
+        mem::drop(output);
+        assert_eq!(c.get(), 7);
+    }
+
+    #[test]
+    fn test_transmute_unchecked_bytes() {
+        fn to_bytes<T>(x: T) -> [u8; 8] {
+            unsafe { transmute_unchecked(x) }
+        }
+
+        fn from_bytes<T>(x: [u8; 8]) -> T {
+            unsafe { transmute_unchecked(x) }
+        }
+
+        let x: u64 = 8932489732;
+        let y: u64 = from_bytes(to_bytes(x));
+        assert_eq!(x, y)
+    }
+
+    #[test]
+    #[cfg(debug_assertions)]
+    #[should_panic]
+    fn test_transmute_wrong_sizes() {
+        let _: u64 = unsafe { transmute_unchecked(89u32) };
+    }
+
+    #[test]
+    #[cfg(debug_assertions)]
+    #[should_panic]
+    fn test_transmute_wrong_alignment() {
+        let xs: [u8; 8] = unsafe { transmute_unchecked(0u64) };
+        let _: u32 = unsafe { transmute_unchecked(&xs[1..5]) };
     }
 }

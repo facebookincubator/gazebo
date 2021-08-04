c8008eceae0c042939694092292d4b0abcf60c42
Neil Mitchell <ndmitchell@fb.com>
2021-08-04 13:46:38 -0700

    Optimise try_map/into_try_map
    Summary: The default Rust code uses the minimum size bound, which is always 0 for a collect::<Result> operation. We can do a lot better, ensuring we allocate only once, and a perfectly tight bound on the allocation.
    
    Reviewed By: krallin
    
    Differential Revision: D30068137
    
    fbshipit-source-id: afc16aeb57fe503715595c823a61a04c8ea5ea18

--- gazebo/src/ext/vec.rs
+++ gazebo/src/ext/vec.rs
@@ -10,6 +10,29 @@
 use crate::{dupe::Dupe, types::TEq};
 use std::borrow::Borrow;
 
+/// Optimised collect iterator into Vec, which might be a Result.
+///
+/// If we do a standard .collect() on the iterator it will never have a good size hint,
+/// as the lower bound will always be zero, so might reallocate several times.
+/// We know the Vec will either be thrown away, or exactly `len`, so aim if we do allocate,
+/// make sure it is at `len`. However, if the first element throws an error, we don't need
+/// to allocate at all, so special case that.
+fn collect_result<T, E>(mut it: impl ExactSizeIterator<Item = Result<T, E>>) -> Result<Vec<T>, E> {
+    match it.next() {
+        None => Ok(Vec::new()),
+        Some(Err(e)) => Err(e),
+        Some(Ok(x)) => {
+            // +1 for the element we have already consumed
+            let mut res = Vec::with_capacity(it.len() + 1);
+            res.push(x);
+            for x in it {
+                res.push(x?);
+            }
+            Ok(res)
+        }
+    }
+}
+
 /// Extension traits on slices/[`Vec`](Vec).
 pub trait SliceExt {
     type Item;
@@ -110,7 +133,7 @@ impl<T> SliceExt for [T] {
     where
         F: FnMut(&'a Self::Item) -> Result<B, E>,
     {
-        self.iter().map(f).collect()
+        collect_result(self.iter().map(f))
     }
 
     fn as_singleton(&self) -> Option<&T> {
@@ -277,6 +300,6 @@ impl<T> VecExt for Vec<T> {
     where
         F: FnMut(Self::Item) -> Result<B, E>,
     {
-        self.into_iter().map(f).collect()
+        collect_result(self.into_iter().map(f))
     }
 }
